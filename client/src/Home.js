ad4eb567d1296b56379e1c036d52a4ed82701913
Andrehlb <andreh.72@hotmail.com>
2024-06-12 16:09:13 -0300

    feat(js files): Create App and Home files.

---
+++ client/src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Bem vindo à Loja de Artigos Esportivos</h1>
+            <p>Aqui você encontra os melhores itens para teu esporte e lazer.</p>
+        </div>
+    );
+};
+
+export default Home;
