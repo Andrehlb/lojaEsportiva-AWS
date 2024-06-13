41398aed51a04aa6a20f614c944b9fc53f94725f
Andrehlb <andreh.72@hotmail.com>
2024-06-13 17:07:55 -0300

    feat(Home.js): update file.

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ProductForm from './ProductForm';
+import ModalForm from './ModalForm';
 
 const Home = () => {
+    const[isModalOpen, setIsModalOpen] = useState(false);
+
+    const handleOpenModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+    };
+
     return (
         <div>
             <h1>Bem vindo à Loja de Artigos Esportivos</h1>
             <p>Aqui você encontra os melhores itens para teu esporte e lazer.</p>
+            <button onClick={handleOpenModal}>Novo Item</button>
+            {isModalOpen && <ModalForm onClose={handleCloseModal}/>}
             <ProductForm />
         </div>
     );
