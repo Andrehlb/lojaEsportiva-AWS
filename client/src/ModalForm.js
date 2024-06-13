5d1f71e205d8a1d944c00401ad9d7fd444640224
Andrehlb <andreh.72@hotmail.com>
2024-06-13 16:49:06 -0300

    feat(ModalForm): update file.

--- client/src/ModalForm.js
+++ client/src/ModalForm.js
@@ -1,1 +1,32 @@
-import 
+import React from 'react';
+
+const ModalForm = () => {
+    const [item, setItem] = useState({
+        name: '',
+    });
+
+    const handleChange = (e) => {
+        setItem({
+            ...item,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Adicionar aqui o código para enviar para o back-end
+        console.log(item);
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Nome do item:
+                <input type="text" name="name" value={item.name}
+onChange={handleChange} required />
+            </label>
+        </form>
+    );
+};
+
+export default ModalForm;
