49c03ba3c2f22ea8c29cc5fdd48568cf19bbc3cd
Andrehlb <andreh.72@hotmail.com>
2024-07-10 04:26:43 -0300

    feat(file): Create `tailwind.config.js`.

---
+++ client/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false,
+  content: [],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
